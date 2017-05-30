27ea73c847a5460a509d699b6a20506fd945d8c2
Vitor Bichara <vitbolb1@gmail.com>
2017-05-29 23:45:01 -0300

    barra de hp e armor

--- client/Entity.ts
+++ client/Entity.ts
@@ -16,7 +16,11 @@ module Kodo {
         dataq: EntityData;
 
         isHost : boolean;
-        hpBar: Phaser.Image;
+
+        hpBar : HealthBar;
+        armorBar : ArmorBar;
+
+        barGroup : Phaser.Group;
 
         constructor(game: Phaser.Game, tile : Tile, id : number, isHost, texture : string, data : EntityData) {
             super(game, tile.x, tile.y, texture);
@@ -26,61 +30,44 @@ module Kodo {
             this.dataq = data;
             this.isHost = isHost;
 
-            var line: Phaser.Line;
-            var graphicsLine;
-           
-            line = new Phaser.Line(0, 0, 0, GameConfig.tileSize == 64 ? 45 : 26);
-   
-            graphicsLine = game.make.graphics(0, 0);
-            graphicsLine.lineStyle(GameConfig.tileSize == 64 ? 6 : 4, 0xd42a2a, 1);
-            graphicsLine.moveTo(line.start.x, line.start.y);
-            graphicsLine.lineTo(line.end.x, line.end.y);
-            graphicsLine.endFill();
-
-
-            this.hpBar = game.add.image(line.start.x, line.start.y, graphicsLine.generateTexture());
-            this.hpBar.anchor.setTo(0.5, 0.5);
-            graphicsLine.destroy();
-
-            this.hpBar.visible = false;
+            this.armorBar = new ArmorBar(this.game, this);
+            this.hpBar = new HealthBar(this.game, this);
+            this.barGroup = this.game.add.group();
+            this.barGroup.add(this.hpBar);
+            this.barGroup.add(this.armorBar);
 
             game.add.existing(this);
         }
 
 
         update() {
-            if (this.hpBar.visible) {
-                if (this.isHost) {
-                    this.hpBar.x = this.x + GameConfig.tileSize/12;
-                    this.hpBar.y = this.y + GameConfig.tileSize / 2;
-                }
-                else {
-                    this.hpBar.x = this.x + GameConfig.tileSize - GameConfig.tileSize/8;
-                    this.hpBar.y = this.y + GameConfig.tileSize / 2;
-                }
-            }
+            //if (this.dataq.armor/this.dataq.maxArmor)
         }
 
         updateStep(newData : EntityData, tile? : Tile) {
-            if ((newData.hp < this.dataq.hp) || newData.armor < this.dataq.armor) {
+            if (newData.hp < this.dataq.hp || newData.armor < this.dataq.armor) {
+                this.armorBar.receiveDamage(newData.armor);
+                this.hpBar.receiveDamage(newData.hp);
+                console.log("hp lenght: "+this.hpBar.lenght+", armor lenght: "+this.armorBar.lenght);
+                if (this.hpBar.lenght < this.armorBar.lenght)
+                    this.barGroup.moveUp(this.armorBar);
+                else 
+                    this.barGroup.moveUp(this.hpBar);
+
                 this.receiveDamage();
-                //this.game.time.events.add(400, this.receiveDamage.bind(this), this);
             }
+
             this.dataq = newData;
         }
         receiveDamage() {
             this.tint = 0xff3030;
-            this.hpBar.visible = true;
             this.game.time.events.add(200, this.resetColor.bind(this), this);
-            this.game.time.events.add(3500, this.hideBar.bind(this), this);
-        }
-        hideBar() {
-            this.hpBar.visible = false;
         }
         resetColor() {
             this.tint = 0xFFFFFF;
         }
         onDeath() {
+            this.armorBar.destroy();
             this.hpBar.destroy();
             this.destroy();
         }
