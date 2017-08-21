377a4212fc60d790c62dc802ca2c792c2d69ee9b
Vitor Bichara <vitbolb1@gmail.com>
2017-08-20 22:28:03 -0300

    balance

--- server/GameConfig.js
+++ server/GameConfig.js
@@ -8,6 +8,6 @@ var GameConfig;
     GameConfig.STARTING_GOLD = 150;
     GameConfig.STARTING_WOOD = 0;
     GameConfig.STARTING_INCOME = 10;
-    GameConfig.STARTING_INCOME_RATE = 17;
+    GameConfig.STARTING_INCOME_RATE = 13;
     GameConfig.BALL_SPAM_RATE = 80;
 })(GameConfig = exports.GameConfig || (exports.GameConfig = {}));
