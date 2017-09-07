25fdab0979d30d7d635ee51b35863bfdf5fc4e23
Vitor Bichara <vitbolb1@gmail.com>
2017-09-07 10:06:35 -0300

    q

--- server/GameConfig.ts
+++ server/GameConfig.ts
@@ -3,8 +3,8 @@ export module GameConfig {
     export const GRID_COLS = 31;
     export const STEP_RATE = 700;//550 700
 
-    export const STARTING_GOLD = 350;
-    export const STARTING_WOOD = 300;
+    export const STARTING_GOLD = 150;
+    export const STARTING_WOOD = 0;
     export const STARTING_INCOME = 15;
     export const STARTING_INCOME_RATE = 14;
     export const BALL_SPAM_RATE = 80;
